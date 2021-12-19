ddc5e2b037ca2ff8ea0882614186f7eec06859ef
Crowdbotics <team@crowdbotics.com>
2021-12-19 20:41:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "raspberry_pi_contro_32492",
+  url: "https://raspberry_pi_contro_32492.botics.co",
+  api: "https://raspberry_pi_contro_32492.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
